3429127078a9c901e269231567f0877abfff3ca9
Bess Sadler <bess.sadler@princeton.edu>
2024-03-26 08:17:26 -0400

    Read a VCF file into a dataframe (#6)

--- tests/test_unique_svs.py
+++ tests/test_unique_svs.py
@@ -2,16 +2,18 @@
 import pytest
 import os
 from src.unique_svs import UniqueSvs
+import pdb
 
-def test_action():
-    testfile = os.path.relpath("fixtures/GDN.set1.fixture.tsv", start=os.curdir)
-    vcf_unique_svs = UniqueSvs()
-    assert "hello_world" == "hello_world"
-    assert "foo" != "bar"
 
-# @pytest.fixture
+def test_create_dataframe():
+    fixture_file = "tests/fixtures/GDN.set1.fixture.tsv"
+    vcf_unique_svs = UniqueSvs(fixture_file)
+    dataframe = vcf_unique_svs.dataframe
+    assert(dataframe.size) == 11160
+
+# 
 # def test_action():
 #     print(testfile)
 #     assert(testfile == "foo")
-#     # vcf_unique_svs = UniqueSvs(file: )
+#     # 
 #     # assert vcf_unique_svs.action(1) == 1
