00aebec2e53c2b5ba767cb0c92f86a35c31f5700
Bess Sadler <bess.sadler@princeton.edu>
2024-03-26 10:21:30 -0400

    Get the sample ids from the VCF file (#8)

--- tests/test_unique_svs.py
+++ tests/test_unique_svs.py
@@ -11,6 +11,20 @@ def test_create_dataframe():
     assert (dataframe.size) == 11160
 
 
+def test_get_sample_column_names():
+    fixture_file = "tests/fixtures/GDN.set1.fixture.tsv"
+    vcf_unique_svs = UniqueSvs(fixture_file)
+    assert (vcf_unique_svs.sample_ids()) == {
+        "GDN41BL",
+        "GDN131BL",
+        "GDN71BL",
+        "GDN111BL",
+        "GDN101BL",
+        "GDN51BL",
+        "GDN91BL",
+    }
+
+
 #
 # def test_action():
 #     print(testfile)
